b22e0c60f75c4d57afd51191004ce3c7cb1c2efd
vickiflama <116015637+vickiflama@users.noreply.github.com>
2022-11-10 18:42:45 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+function inclusivo() {
+  document.body.style.color = "none";
+  document.body.style.color ="white";
+  document.body.style.backgroundImage = "none";
+  document.body.style.backgroundColor = "black";
+document.getElementById ("header").style.backgroundColor = "black";
+document.getElementById("footer").style.backgroundColor = "black";
+document.getElementById("titulito").style.color = "white";
+}
+
+function datosDelCreador() {
+  alert ("Autora: Victoria Flama; Santa Fe, Capital. INSTAGRAM: vicoflama")
+}
